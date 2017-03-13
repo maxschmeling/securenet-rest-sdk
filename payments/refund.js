c6db898c10c5c4d8ed9e78b10a9148b22e8cf35c
Max Schmeling <schmeling88@gmail.com>
2017-03-13 01:54:40 -0500

    Fixed typo in refund method.

--- payments/refund.js
+++ payments/refund.js
@@ -1,7 +1,7 @@
 const { POST, getObjectFromResponse } = require('../util');
 
 module.exports = async function refund(config, transactionId, amount = null) {
-  const respone = await POST(config, 'Payments/Refund', { transactionId, amount });
+  const response = await POST(config, 'Payments/Refund', { transactionId, amount });
 
   return getObjectFromResponse('transaction', response);
 }
