50eab48c4bbecb2bdbd5b2e08fc4ed7f26638f45
Max Schmeling <schmeling88@gmail.com>
2017-03-13 02:12:17 -0500

    Only send amount if not null in refund.

--- payments/refund.js
+++ payments/refund.js
@@ -1,7 +1,11 @@
 const { POST, getObjectFromResponse } = require('../util');
 
 module.exports = async function refund(config, transactionId, amount = null) {
-  const response = await POST(config, 'Payments/Refund', { transactionId, amount });
+  let body = { transactionId };
+
+  if (amount) body.amount = amount;
+
+  const response = await POST(config, 'Payments/Refund', body);
 
   return getObjectFromResponse('transaction', response);
 }
